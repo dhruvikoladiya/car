74345eb4753bc6cce71e82818302bbca8709d90f
jaydip koladiya <jaydip585@gmail.com>
2022-02-09 11:36:58 +0530

    cor

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ const cors=require('cors')
 const app=express()
 const port=process.env.PORT
 
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200,
+    methods: "GET, PUT, POST, PATCH, DELETE"
+}
 
 app.use(express.json())
-app.use(cors({
-    origin:'*'
-}))
+app.use(cors(corsOptions))
 app.use(userrouter)
 app.use(servicerouter)
 app.use(carrouter)
