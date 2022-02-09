0ac705bc20484bf6cc5f451165b33203c626d05b
jaydip koladiya <jaydip585@gmail.com>
2022-02-09 13:07:14 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -10,12 +10,11 @@ const app=express()
 const port=process.env.PORT
 
 app.use(express.json())
-app.use(cors({
-    origin: 'http://localhost:3000'
-}))
+app.use(cors())
 app.use(userrouter)
 app.use(servicerouter)
 app.use(carrouter)
+
 
 app.listen(port,()=>{
     console.log('Server is up on '+port)
