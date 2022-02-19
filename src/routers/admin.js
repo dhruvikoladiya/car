08a161fa46e741e5488ee6cb8133dba2f721e5e5
jaydip koladiya <jaydip585@gmail.com>
2022-02-19 11:19:38 +0530

    add

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -2,22 +2,21 @@ const express=require('express')
 const router=new express.Router()
 const Admin=require('../model/admin')
 
-// router.post('/registeradmin',async(req,res)=>{
-//     const admin=new Admin(req.body)
-//     res.send(req.body)
-//     try{
-//         await admin.save()
-//         res.status(201).json({admin})
-//     }catch(e){
-//         res.status(400).json({error:e})
-//     } 
-// })
+router.post('/registeradmin',async(req,res)=>{
+    const admin=new Admin(req.body)
+    res.send(req.body)
+    try{
+        await admin.save()
+        res.status(201).json({admin})
+    }catch(e){
+        res.status(400).json({error:e})
+    } 
+})
 
 router.post('/loginadmin',async(req,res)=>{
 
     try{
         const admin=await Admin.findByCredentials(req.body.id,req.body.password)
-        res.status(200).json({admin})
     }catch(e){
         res.status(400).json({error:"Email and password does not match!"})
     }
